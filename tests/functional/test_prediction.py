8ecd5b0266b9c4c7681e6040f16f83995829f3b1
robson-koji <rbsnkjmr@gmail.com>
2022-09-06 19:14:37 -0300

    start testing prediction

--- tests/functional/test_prediction.py
+++ tests/functional/test_prediction.py
@@ -3,15 +3,40 @@ from src import dedupe_gazetteer as dg
 from src import dedupe_gazetteer_utils as du
 
 
-import _io, os
+import os
 import unittest
-import dedupe
-
+from datetime import datetime
 
 from settings import *
 
-class TestModelEvaluation(unittest.TestCase):
-    def test_init(self):
-        """ Test ModelEvaluation __init__ method """
+class TestPredictionProcess(unittest.TestCase):
+    def setUp(self) -> None:
+        self.model_evaluation = dg.ModelEvaluation(ip_canonical_file, ip_messy_test_file, op_false_positives_file, op_false_negatives_file)
+        self.noisify = dg.Noisify(1, 1)
+        return super().setUp()
+
+    def test_noisify(self):
+        noisify = dg.Noisify(1, 1)
+        self.assertIsInstance(noisify, dg.Noisify)
+
+    def test_training_test(self):
+        tt = dg.TrainingTest(ip_canonical_file, ip_messy_test_file, op_settings_file, op_matches_found_file, self.model_evaluation, self.noisify)
+
+
+    def test_prediction(self):
+        ### Prediction
+        step_time = datetime.now()
         model_evaluation = dg.ModelEvaluation(ip_canonical_file, ip_messy_test_file, op_false_positives_file, op_false_negatives_file)
-        self.assertIsInstance(model_evaluation, dg.ModelEvaluation)
+        tt = dg.TrainingTest(ip_canonical_file, ip_messy_test_file, op_settings_file, op_matches_found_file, model_evaluation)
+        tt.cluster_data()
+        end_time = datetime.now()
+        print(f"Prediction Time and File Recording Time: {end_time - step_time} \n")
+        ###
+
+    def evaluate_model(self):
+        ### Performance of the algorithm
+        step_time = datetime.now()
+        tt.evaluate_model()
+        end_time = datetime.now()
+        print(f"Model Evaluation Time and Files Recording Time: {end_time - step_time} \n")
+        ###
